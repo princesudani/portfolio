fe9be0bf35a8d8080797c67e5e216d4d803a1fdf
princesudani <princesudani0610@gmail.com>
2023-12-12 14:46:02 +0530

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,17 @@ import React from "react";
 
 const Header = () => {
   return (
-      <>
+    <>
       <header className="text-gray-400 bg-gray-900 body-font">
         <div className=" mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          
           <a className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-white lg:items-center lg:justify-center mb-4 md:mb-0">
-          {/* <img src="image/logo.png" alt="Logo" className="object-contain hover:object-scale-down"/> */}
-          <h1 className="text-4xl">ᑭᖇIᑎᑕE</h1>
+            <img
+              src="./image/prince.png"
+              alt="Logo"
+              className="object-contain hover:object-scale-down"
+            />
+            {/* <h1 className="text-4xl">ᑭᖇIᑎᑕE</h1> */}
           </a>
-
         </div>
       </header>
     </>
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -5,11 +5,10 @@ const Testimonial = () => {
     <>
       <section className="text-gray-400 bg-gray-900 body-font">
         <div className=" px-5 py-24 mx-auto">
-        <div className="flex flex-col text-center w-full mb-12">
+          <div className="flex flex-col text-center w-full mb-12">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">
               Project
             </h1>
-          
           </div>
           <div className="flex flex-wrap -m-4">
             <div className="p-4 md:w-1/2">
@@ -20,15 +19,15 @@ const Testimonial = () => {
                   alt="Currency Image"
                 />
                 <div className="p-6">
-                  
                   <h1 className="title-font text-lg font-medium text-white mb-3">
                     Currency Converter
                   </h1>
                   <p className="leading-relaxed mb-3">
-                  React project: Currency converter for seamless global financial transactions.
+                    React project: Currency converter for seamless global
+                    financial transactions.
                   </p>
                   <div className="flex items-center flex-wrap ">
-                    <a className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
+                    <a href="https://currency-converter-princesudani.vercel.app/" target="_blank" className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
                       Learn More
                       <svg
                         className="w-4 h-4 ml-2"
@@ -55,15 +54,14 @@ const Testimonial = () => {
                   alt="blog"
                 />
                 <div className="p-6">
-                  
                   <h1 className="title-font text-lg font-medium text-white mb-3">
                     Latest NEWS
                   </h1>
                   <p className="leading-relaxed mb-3">
-                  React project: Stay informed with a sleek Latest News App.
+                    React project: Stay informed with a sleek Latest News App.
                   </p>
                   <div className="flex items-center flex-wrap ">
-                    <a className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
+                    <a href="https://news-eosin-omega.vercel.app/" target="_blank" className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
                       Learn More
                       <svg
                         className="w-4 h-4 ml-2"
@@ -82,7 +80,6 @@ const Testimonial = () => {
                 </div>
               </div>
             </div>
-        
           </div>
         </div>
       </section>
